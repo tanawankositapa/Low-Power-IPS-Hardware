960a12d4d72c1147cf71ca8733825c0ca7e58ea8
mone0371 <60010371@kmitl.ac.th>
2020-10-07 17:59:57 +0700

    Update BLE_Scanner

---
+++ BLE-Projects/BLE_Scanner/Output/ble_heart_rate Debug/Obj/aes.d
@@ -0,0 +1,14 @@
+Output/ble_heart_rate\ Debug/Obj/aes.o: \
+ D:\Low\ Power\ Platform\Low-Power-IPS-Hardware\BLE-Projects\nRF5_SDK_current\external\mbedtls\library\aes.c \
+ ../nRF5_SDK_current/external/nrf_tls/mbedtls/nrf_crypto/config/nrf_crypto_mbedtls_config.h \
+ sdk_config.h \
+ ../nRF5_SDK_current/external/mbedtls/include/mbedtls/check_config.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/limits.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/string.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/__crossworks.h \
+ ../nRF5_SDK_current/external/mbedtls/include/mbedtls/aes.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/stddef.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/stdint.h \
+ ../nRF5_SDK_current/external/mbedtls/include/mbedtls/platform.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/stdlib.h \
+ ../nRF5_SDK_current/external/mbedtls/include/mbedtls/platform_util.h
---
+++ BLE-Projects/BLE_Scanner/Output/ble_heart_rate Debug/Obj/app_util_platform.d
@@ -0,0 +1,31 @@
+Output/ble_heart_rate\ Debug/Obj/app_util_platform.o: \
+ D:\Low\ Power\ Platform\Low-Power-IPS-Hardware\BLE-Projects\nRF5_SDK_current\components\libraries\util\app_util_platform.c \
+ D:\Low\ Power\ Platform\Low-Power-IPS-Hardware\BLE-Projects\nRF5_SDK_current\components\libraries\util\app_util_platform.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/stdint.h \
+ ../nRF5_SDK_current/modules/nrfx/mdk/compiler_abstraction.h \
+ ../nRF5_SDK_current/modules/nrfx/mdk/nrf.h \
+ ../nRF5_SDK_current/modules/nrfx/mdk/nrf52840.h \
+ ../nRF5_SDK_current/components/toolchain/cmsis/include/core_cm4.h \
+ ../nRF5_SDK_current/components/toolchain/cmsis/include/cmsis_version.h \
+ ../nRF5_SDK_current/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../nRF5_SDK_current/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../nRF5_SDK_current/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../nRF5_SDK_current/modules/nrfx/mdk/system_nrf52840.h \
+ ../nRF5_SDK_current/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../nRF5_SDK_current/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../nRF5_SDK_current/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../nRF5_SDK_current/modules/nrfx/mdk/compiler_abstraction.h \
+ ../nRF5_SDK_current/components/softdevice/s140/headers/nrf_soc.h \
+ ../nRF5_SDK_current/components/softdevice/s140/headers/nrf_svc.h \
+ ../nRF5_SDK_current/components/softdevice/s140/headers/nrf_error.h \
+ ../nRF5_SDK_current/components/softdevice/s140/headers/nrf_error_soc.h \
+ ../nRF5_SDK_current/components/softdevice/s140/headers/nrf_nvic.h \
+ D:\Low\ Power\ Platform\Low-Power-IPS-Hardware\BLE-Projects\nRF5_SDK_current\components\libraries\util\nrf_assert.h \
+ D:\Low\ Power\ Platform\Low-Power-IPS-Hardware\BLE-Projects\nRF5_SDK_current\components\libraries\util\app_error.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/stdio.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/__crossworks.h \
+ C:/Users/mone/ncs/v1.3.0/toolchain/segger_embedded_studio/include/stdbool.h \
+ D:\Low\ Power\ Platform\Low-Power-IPS-Hardware\BLE-Projects\nRF5_SDK_current\components\libraries\util\sdk_errors.h \
+ ../nRF5_SDK_current/components/softdevice/s140/headers/nrf_error.h \
+ D:\Low\ Power\ Platform\Low-Power-IPS-Hardware\BLE-Projects\nRF5_SDK_current\components\libraries\util\nordic_common.h \
+ D:\Low\ Power\ Platform\Low-Power-IPS-Hardware\BLE-Projects\nRF5_SDK_current\components\libraries\util\app_error_weak.h
