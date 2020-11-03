93d9a42b3cbefbaf9dfb88ddefea622c962cf8df
mone0371 <60010371@kmitl.ac.th>
2020-11-03 15:48:04 +0700

    add murata borard file

---
+++ STM32CubeExpansion_LRWAN_V1.3.1/Projects/B-L072Z-LRWAN1/Applications/LoRa/AT_Slave/MDK-ARM/mlm32l07x01/system_stm32l0xx.d
@@ -0,0 +1,10 @@
+.\mlm32l07x01\system_stm32l0xx.o: ..\..\..\..\..\..\Drivers\CMSIS\Device\ST\STM32L0xx\Source\Templates\system_stm32l0xx.c
+.\mlm32l07x01\system_stm32l0xx.o: ..\..\..\..\..\..\Drivers\CMSIS\Device\ST\STM32L0xx\Include\stm32l0xx.h
+.\mlm32l07x01\system_stm32l0xx.o: ..\..\..\..\..\..\Drivers\CMSIS\Device\ST\STM32L0xx\Include\stm32l072xx.h
+.\mlm32l07x01\system_stm32l0xx.o: ..\..\..\..\..\..\Drivers\CMSIS\Include\core_cm0plus.h
+.\mlm32l07x01\system_stm32l0xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\mlm32l07x01\system_stm32l0xx.o: ..\..\..\..\..\..\Drivers\CMSIS\Include\cmsis_version.h
+.\mlm32l07x01\system_stm32l0xx.o: ..\..\..\..\..\..\Drivers\CMSIS\Include\cmsis_compiler.h
+.\mlm32l07x01\system_stm32l0xx.o: ..\..\..\..\..\..\Drivers\CMSIS\Include\cmsis_armcc.h
+.\mlm32l07x01\system_stm32l0xx.o: ..\..\..\..\..\..\Drivers\CMSIS\Include\mpu_armv7.h
+.\mlm32l07x01\system_stm32l0xx.o: ..\..\..\..\..\..\Drivers\CMSIS\Device\ST\STM32L0xx\Include\system_stm32l0xx.h
---
+++ STM32CubeExpansion_LRWAN_V1.3.1/Projects/B-L072Z-LRWAN1/Applications/LoRa/AT_Slave/MDK-ARM/mlm32l07x01/tiny_vsnprintf.d
@@ -0,0 +1,4 @@
+.\mlm32l07x01\tiny_vsnprintf.o: ..\LoRaWAN\App\src\tiny_vsnprintf.c
+.\mlm32l07x01\tiny_vsnprintf.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
+.\mlm32l07x01\tiny_vsnprintf.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\mlm32l07x01\tiny_vsnprintf.o: ..\LoRaWAN\App\inc\tiny_vsnprintf.h
